1198a7f8045b91a35af6c713dafd88d8dc775c91
YeHyun Byeon <oopptteh@gmail.com>
2024-01-04 10:55:30 +0900

    FEAT:

--- lib/pages/main page/mainPage.dart
+++ lib/pages/main page/mainPage.dart
@@ -9,11 +9,24 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('나의 하루'),
       ),
-        body: CalendarDatePicker(
-          initialDate: DateTime.now(),
-          firstDate: DateTime(1900),
-          lastDate: DateTime(2300),
-          onDateChanged: (value) {},
+        body: Column(
+          children: [
+            CalendarDatePicker(
+              initialDate: DateTime.now(),
+              firstDate: DateTime(1900),
+              lastDate: DateTime(2300),
+              onDateChanged: (value) {},
+            ),
+            ListView.builder(itemBuilder: (context, index) => const ListTile(
+              title: Text('asdf'),
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: null,
+                ),
+              ),
+              itemCount: 100,
+            )
+          ],
         ),
     );
   }
